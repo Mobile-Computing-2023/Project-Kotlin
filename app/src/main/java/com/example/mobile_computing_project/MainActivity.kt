25b84f5bed28f6ff02ad4b53ce6f39095a08ce72
aryanteng <aryanteng892@gmail.com>
2023-03-06 00:17:51 +0530

    init project

---
+++ app/src/main/java/com/example/mobile_computing_project/MainActivity.kt
@@ -0,0 +1,11 @@
+gitpackage com.example.mobile_computing_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
