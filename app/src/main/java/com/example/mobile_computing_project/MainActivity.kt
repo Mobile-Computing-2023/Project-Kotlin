7fdefe6d46cfe8f05780d5415e3cac7cd047d255
aryanteng <aryanteng892@gmail.com>
2023-03-06 00:49:20 +0530

    added dependencies for firebase

--- app/src/main/java/com/example/mobile_computing_project/MainActivity.kt
+++ app/src/main/java/com/example/mobile_computing_project/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.mobile_computing_project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mobile_computing_project.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
