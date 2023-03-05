13345b92f1643024d28524d733bf34f0d194ed33
aryanteng <aryanteng892@gmail.com>
2023-03-06 00:54:55 +0530

    implemented user login

--- app/src/main/java/com/example/mobile_computing_project/MainActivity.kt
+++ app/src/main/java/com/example/mobile_computing_project/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.mobile_computing_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.mobile_computing_project.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
+private const val TAG = "LoginActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
@@ -13,5 +19,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        auth = Firebase.auth
+        if (auth.currentUser != null) {
+            goToHomeActivity()
+        }
+
+        binding.btnLogin.setOnClickListener {
+            binding.btnLogin.isEnabled = false
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+            if (email.isBlank() || password.isBlank()) {
+                binding.btnLogin.isEnabled = true
+                Toast.makeText(this, "Please fill the details!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    binding.btnLogin.isEnabled = true
+                    Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
+                    goToHomeActivity()
+                }
+                else {
+                    binding.btnLogin.isEnabled = true
+                    Log.i(TAG, "Sign In Failed", task.exception)
+                    Toast.makeText(this, "Authentication Failed!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    private fun goToHomeActivity(){
+        Log.i(TAG, "goPostsActivity")
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
