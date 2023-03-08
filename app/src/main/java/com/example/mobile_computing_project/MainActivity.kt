7956cfca4750e7543242c329fc738fe4f0d666a5
aryanteng <aryanteng892@gmail.com>
2023-03-09 02:52:38 +0530

    created fragment for canteen and wrote code for conditional render of bottom bar

--- app/src/main/java/com/example/mobile_computing_project/MainActivity.kt
+++ app/src/main/java/com/example/mobile_computing_project/MainActivity.kt
@@ -2,15 +2,21 @@ package com.example.mobile_computing_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.mobile_computing_project.databinding.ActivityMainBinding
-import com.example.mobile_computing_project.fragments.CartFragment
-import com.example.mobile_computing_project.fragments.ComplaintFragment
-import com.example.mobile_computing_project.fragments.MenuFragment
-import com.example.mobile_computing_project.fragments.ProfileFragment
+import com.example.mobile_computing_project.fragments.*
+import com.example.mobile_computing_project.models.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var signedInUser: User? = null
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,42 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         replaceFragment(MenuFragment())
+
+        auth = Firebase.auth
+        val db = Firebase.firestore
+
+        db.collection("Users").document(auth.currentUser?.uid as String).get().addOnSuccessListener {
+            signedInUser = it.toObject(User::class.java)!!
+            Log.i(TAG, "Signed In User: $signedInUser")
+        }.addOnFailureListener {error ->
+            Log.i(TAG, "Failure in fetching current user", error)
+        }
+
+        // for future use
+//        if(signedInUser?.isCanteen == true){
+//            binding.bottomNavView.setOnItemSelectedListener {
+//                when(it.itemId){
+//                    R.id.menu -> replaceFragment(CanteenMenuFragment())
+//                    R.id.orders -> replaceFragment(OrdersFragment())
+//                    R.id.complaint -> replaceFragment(CanteenComplaintFragment())
+//                    R.id.specials -> replaceFragment(SpecialsFragment())
+//                    else -> {}
+//                }
+//                true
+//            }
+//        }
+//        else{
+//            binding.bottomNavView.setOnItemSelectedListener {
+//                when(it.itemId){
+//                    R.id.menu -> replaceFragment(MenuFragment())
+//                    R.id.cart -> replaceFragment(CartFragment())
+//                    R.id.complaint -> replaceFragment(ComplaintFragment())
+//                    R.id.profile -> replaceFragment(ProfileFragment())
+//                    else -> {}
+//                }
+//                true
+//            }
+//        }
 
         binding.bottomNavView.setOnItemSelectedListener {
             when(it.itemId){
--- app/src/main/java/com/example/mobile_computing_project/models/User.kt
+++ app/src/main/java/com/example/mobile_computing_project/models/User.kt
@@ -1,3 +1,4 @@
 package com.example.mobile_computing_project.models
 
-data class User(var uid: String = "", var name: String = "", var email: String = "")
+data class User(var uid: String = "", var name: String = "", var email: String = "", var isCanteen: Boolean = false) {
+}
