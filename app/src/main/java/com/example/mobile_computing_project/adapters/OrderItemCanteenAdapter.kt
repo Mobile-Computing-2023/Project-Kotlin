04acd1ffad6a189af3a9a28aff21bc39e45e72ed
aryanteng <aryanteng892@gmail.com>
2023-03-11 01:09:21 +0530

    cleaned messy code of order item canteen adapter removed useless variable context and for loop

--- app/src/main/java/com/example/mobile_computing_project/adapters/OrderItemCanteenAdapter.kt
+++ app/src/main/java/com/example/mobile_computing_project/adapters/OrderItemCanteenAdapter.kt
@@ -19,7 +19,7 @@ class OrderItemCanteenAdapter (private val orderItems: List<OrderItem>):
     private lateinit var tvStatus: TextView
     private lateinit var tvCreatedAt: TextView
     private lateinit var rvItems: RecyclerView
-    private lateinit var bekaarContext: Context
+    private var canteenOrderItems: MutableList<CartItem> = mutableListOf()
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         fun bind(orderItem: OrderItem){
@@ -27,28 +27,20 @@ class OrderItemCanteenAdapter (private val orderItems: List<OrderItem>):
             tvAmount.text = "Order Total:\nRs " + orderItem.amount.toString()
             tvStatus.text = "status: " + orderItem.status
             tvCreatedAt.text = DateUtils.getRelativeTimeSpanString(orderItem.createdAt)
-
-            val canteenOrderItems: MutableList<CartItem> = mutableListOf()
-            val itemsList = orderItem.items?.toMutableList<CartItem>()
-            println(itemsList)
-            if (itemsList != null) {
-                for (i in itemsList){
-                    canteenOrderItems.add(i)
-                }
-            }
+            canteenOrderItems = orderItem.items as MutableList<CartItem>
             rvItems.adapter = CanteenOrderItemsAdapter(canteenOrderItems)
-            rvItems.layoutManager = LinearLayoutManager(bekaarContext)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.order_item_canteen, parent, false)
-        bekaarContext = parent.context
         tvName = view.findViewById(R.id.tv_name)
         tvAmount = view.findViewById(R.id.tv_amount)
         tvStatus = view.findViewById(R.id.tv_status)
         tvCreatedAt = view.findViewById(R.id.tv_createdAt)
         rvItems = view.findViewById(R.id.rv_canteen_orders_items)
+        rvItems.layoutManager = LinearLayoutManager(parent.context)
+
         return ViewHolder(view)
     }
 
