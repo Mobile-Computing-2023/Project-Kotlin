5a6f497ce5cd0d7e0fb81908f13f28c165e01274
aryanteng <aryanteng892@gmail.com>
2023-04-01 22:09:39 +0530

    add item to menu with image successful

--- app/src/main/java/com/example/mobile_computing_project/fragments/AddMenuItemFragment.kt
+++ app/src/main/java/com/example/mobile_computing_project/fragments/AddMenuItemFragment.kt
@@ -1,19 +1,26 @@
 package com.example.mobile_computing_project.fragments
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.Toast
 import android.widget.ToggleButton
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.DialogFragment
 import com.example.mobile_computing_project.R
 import com.example.mobile_computing_project.models.MenuItem
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import java.util.UUID
 
 class AddMenuItemFragment: DialogFragment() {
@@ -21,11 +28,23 @@ class AddMenuItemFragment: DialogFragment() {
     private lateinit var btnConfirmAddItem: Button
     private lateinit var etItemName: EditText
     private lateinit var etItemPrice: EditText
-    private lateinit var etItemServingSize: EditText
-//    private lateinit var rbVeg: RadioButton
-//    private lateinit var rbNonVeg: RadioButton
+    private lateinit var btnChooseFile: Button
     private lateinit var tbVeg: ToggleButton
+    private lateinit var ivImage: ImageView
     var isSpl = false
+    private var photoURI: Uri? = null
+    private val getResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode == Activity.RESULT_OK){
+                photoURI = it.data?.data
+                Log.i("data", "$photoURI")
+                ivImage.setImageURI(photoURI)
+            }
+            else{
+                Toast.makeText(context, "Image Picking Action Canceled", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,10 +54,12 @@ class AddMenuItemFragment: DialogFragment() {
         val view = inflater.inflate(R.layout.add_item_canteen, container, false)
         btnDismiss = view.findViewById(R.id.btn_dismiss)
         btnConfirmAddItem = view.findViewById(R.id.btn_add_item)
+        btnChooseFile = view.findViewById(R.id.btn_choose_file)
         etItemName = view.findViewById(R.id.et_name)
         etItemPrice = view.findViewById(R.id.et_price)
         tbVeg = view.findViewById(R.id.tb_veg_nonveg)
         isSpl = requireArguments().getBoolean("isSpl", false)
+        ivImage= view.findViewById(R.id.iv_img_src)
         return view
     }
 
@@ -49,14 +70,20 @@ class AddMenuItemFragment: DialogFragment() {
         }
 
         btnConfirmAddItem.setOnClickListener {
-            val menuItem = MenuItem(
-                mid = UUID.randomUUID().toString(),
-                name = etItemName.text.toString(),
-                price = etItemPrice.text.toString().toInt(),
-                isVeg = tbVeg.isChecked.not(),
-                special = isSpl
-            )
-            addItemToMenu(menuItem)
+            if(etItemName.text.isEmpty()){
+                Toast.makeText(context, "Enter name!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(etItemPrice.text.isEmpty()){
+                Toast.makeText(context, "Enter price!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(photoURI == null){
+                Toast.makeText(context, "Select an Image!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            btnChooseFile.isEnabled = false
+            addItemToMenu()
         }
 
         tbVeg.setOnCheckedChangeListener { _, isChecked ->
@@ -66,17 +93,38 @@ class AddMenuItemFragment: DialogFragment() {
                 tbVeg.setBackgroundColor(resources.getColor(R.color.veg))
             }
         }
+
+        btnChooseFile.setOnClickListener {
+            val imagePickerIntent = Intent(Intent.ACTION_GET_CONTENT)
+            imagePickerIntent.type = "image/*"
+            getResult.launch(imagePickerIntent)
+        }
     }
 
-    private fun addItemToMenu(menuItem: MenuItem){
-        val db  = Firebase.firestore
-        db.collection("Menu").document(menuItem.mid).set(menuItem)
-            .addOnSuccessListener {
-                Toast.makeText(context, "Added Item", Toast.LENGTH_SHORT).show()
-                dismiss()
-            }
-            .addOnFailureListener {
-                Toast.makeText(context, "There was some error while adding item", Toast.LENGTH_SHORT).show()
-            }
+    private fun addItemToMenu(){
+        val mid = UUID.randomUUID().toString()
+        val storageRef = Firebase.storage.reference
+        val photoRef = storageRef.child("images/${mid}--photo.jpg")
+        val photoTask = photoRef.putFile(photoURI!!)
+        photoTask.continueWithTask { photoRef.downloadUrl }.continueWithTask {
+            val menuItem = MenuItem(
+                mid = mid,
+                name = etItemName.text.toString(),
+                price = etItemPrice.text.toString().toInt(),
+                isVeg = tbVeg.isChecked.not(),
+                special = isSpl,
+                imgSrc = it.result.toString()
+            )
+            val db  = Firebase.firestore
+            db.collection("Menu").document(menuItem.mid).set(menuItem)
+                .addOnSuccessListener {
+                    Toast.makeText(context, "Added Item", Toast.LENGTH_SHORT).show()
+                    dismiss()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(context, "There was some error while adding item", Toast.LENGTH_SHORT).show()
+                }
+        }
+
     }
 }
