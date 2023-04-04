6880636fa82dc3fc065229101eaad7c7ff092aa4
aryanteng <aryanteng892@gmail.com>
2023-04-04 23:11:57 +0530

    added recycler view to order history of user

--- app/src/main/java/com/example/mobile_computing_project/adapters/OrderItemUserAdapter.kt
+++ app/src/main/java/com/example/mobile_computing_project/adapters/OrderItemUserAdapter.kt
@@ -5,22 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobile_computing_project.R
+import com.example.mobile_computing_project.models.CartItem
 import com.example.mobile_computing_project.models.OrderItem
 
 class OrderItemUserAdapter(private val orderItems: List<OrderItem>):
     RecyclerView.Adapter<OrderItemUserAdapter.ViewHolder>() {
     private lateinit var tvAmount: TextView
-    private lateinit var tvItems: TextView
     private lateinit var tvStatus: TextView
     private lateinit var tvCreatedAt: TextView
+    private lateinit var rvItems: RecyclerView
+    private var userOrderItems: MutableList<CartItem> = mutableListOf()
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         fun bind(orderItem: OrderItem){
             tvAmount.text = "Order Total: Rs " + orderItem.amount.toString()
             tvStatus.text = "Status: " + orderItem.status
             tvCreatedAt.text = DateUtils.getRelativeTimeSpanString(orderItem.createdAt)
+            userOrderItems = orderItem.items as MutableList<CartItem>
+            rvItems.adapter = CanteenOrderItemsAdapter(userOrderItems)
         }
     }
 
@@ -28,8 +33,9 @@ class OrderItemUserAdapter(private val orderItems: List<OrderItem>):
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_order_user, parent, false)
         tvAmount = view.findViewById(R.id.tv_amount)
         tvStatus = view.findViewById(R.id.tv_status)
-        tvItems = view.findViewById(R.id.tv_items)
         tvCreatedAt = view.findViewById(R.id.tv_createdAt)
+        rvItems = view.findViewById(R.id.rv_user_orders_items)
+        rvItems.layoutManager = LinearLayoutManager(parent.context)
         return ViewHolder(view)
     }
 
