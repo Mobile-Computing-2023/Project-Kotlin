5c6245d0326d1ceaf6da0174b70b23f92d3b5de6
aryanteng <aryanteng892@gmail.com>
2023-04-08 20:07:24 +0530

    canteen order items adapter

--- app/src/main/java/com/example/mobile_computing_project/adapters/canteen/CanteenOrderItemsAdapter.kt
+++ app/src/main/java/com/example/mobile_computing_project/adapters/canteen/CanteenOrderItemsAdapter.kt
@@ -2,37 +2,32 @@ package com.example.mobile_computing_project.adapters.canteen
 
 import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobile_computing_project.R
+import com.example.mobile_computing_project.databinding.ItemOrderCanteenBinding
+import com.example.mobile_computing_project.databinding.ItemOrderItemBinding
 import com.example.mobile_computing_project.models.CartItem
 
 class CanteenOrderItemsAdapter (private val cartItems: List<CartItem>):
     RecyclerView.Adapter<CanteenOrderItemsAdapter.ViewHolder>() {
-    private lateinit var tvName: TextView
-    private lateinit var ivVeg: ImageView
-    private lateinit var tvQty: TextView
 
-    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(private val binding: ItemOrderItemBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(cartItem: CartItem){
-            tvName.text = cartItem.name.capitalize()
-            tvQty.text = cartItem.qty.toString()
+            binding.tvName.text = cartItem.name.capitalize()
+            binding.tvQty.text = cartItem.qty.toString()
             if (cartItem.isVeg){
                 val color = Color.parseColor("#049D4E")
-                ivVeg.setColorFilter(color)
+                binding.ivVeg.setColorFilter(color)
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_order_item, parent, false)
-        tvName = view.findViewById(R.id.tv_name)
-        ivVeg = view.findViewById(R.id.iv_veg_symbol)
-        tvQty = view.findViewById(R.id.tv_qty)
-        return ViewHolder(view)
+        val binding = ItemOrderItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun getItemCount() = cartItems.size
