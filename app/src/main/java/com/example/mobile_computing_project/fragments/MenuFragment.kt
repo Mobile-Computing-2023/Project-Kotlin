be33afcb00158a3cfa3fb3f6dd4ba45cdf63893b
aryanteng <aryanteng892@gmail.com>
2023-04-15 23:31:04 +0530

    error resolved

--- app/src/main/java/com/example/mobile_computing_project/fragments/MenuFragment.kt
+++ app/src/main/java/com/example/mobile_computing_project/fragments/MenuFragment.kt
@@ -197,7 +197,7 @@ class MenuFragment : Fragment() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun sendNotification() {
-        val builder = Notification.Builder(context, "CHANNEL_ID")
+        val builder = Notification.Builder(activity, "CHANNEL_ID")
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle("Example Title")
             .setContentText("Example Description")
